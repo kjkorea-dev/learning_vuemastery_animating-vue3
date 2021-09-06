b2ae3fbd5469a6fed1976da0a664f1804785b882
kj <agassi77@gmail.com>
2021-09-06 16:52:38 +0900

    Group Transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Modal</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/list">List</router-link>
   </div>
   <router-view v-slot="{ Component }">
     <transition name="slide-fade" mode="out-in">
@@ -33,17 +33,23 @@
 }
 
 button {
-  width: 5em;
-  height: 2em;
+  height: 2.2em;
   margin: 0.5em;
-  border-radius: 5px;
+  border-radius: 0.3em;
   background: linear-gradient(to right, #16c0b0, #84cf6a);
-  font-size: 1em;
   color: white;
-  border: none;
-  outline: none;
-  cursor: pointer;
 }
+
+input {
+  display: inline-block;
+  box-sizing: border-box;
+  width: 30%;
+  height: 1.8em;
+  padding: 0.5em;
+  margin: 0.2em;
+  font: 0.8em 'Avenir', Helvetica, sans-serif;
+}
+
 .modal {
   width: 250px;
   height: 200px;
@@ -52,6 +58,16 @@ button {
   margin-top: 20px;
   border-radius: 2%;
   background-color: #e0e0e0;
+}
+
+p {
+  font-size: 0.8em;
+}
+::placeholder {
+  color: #2c3e50;
+}
+ul {
+  list-style-type: none;
 }
 
 /*** Transitions ***/
@@ -75,5 +91,16 @@ button {
 .slide-fade-leave-to {
   transform: translateX(-10px);
   opacity: 0;
+}
+
+.slide-up-enter-from {
+  transform: translateY(10px);
+  opacity: 0;
+}
+.slide-up-enter-active {
+  transition: all 0.5s ease;
+}
+.slide-up-move {
+  transition: transform 0.5s ease-out;
 }
 </style>
---
+++ src/views/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" v-model="newContact" placeholder="Name" />
+    <button @click="addContact">Add Contact</button>
+    <button @click="sortContacts">Sort</button>
+    <transition-group name="slide-up" tag="ul" appear>
+      <li v-for="contact in contacts" :key="contact">{{ contact }}</li>
+    </transition-group>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      newContact: '',
+      contacts: ['Son', 'Mike', 'Ann Marie'],
+    }
+  },
+  methods: {
+    addContact() {
+      this.contacts.push(this.newContact)
+      this.newContact = ''
+    },
+    sortContacts() {
+      this.contacts.sort()
+    },
+  },
+}
+</script>
