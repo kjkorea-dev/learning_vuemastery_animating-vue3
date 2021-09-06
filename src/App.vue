de67bd0919d37b35e662f9cb6e2ace5fabfba7a7
kj <agassi77@gmail.com>
2021-09-06 14:13:45 +0900

    Setup start code

--- src/App.vue
+++ src/App.vue
@@ -27,4 +27,26 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+button {
+  width: 5em;
+  height: 2em;
+  margin: 0.5em;
+  border-radius: 5px;
+  background: linear-gradient(to right, #16c0b0, #84cf6a);
+  font-size: 1em;
+  color: white;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+.modal {
+  width: 250px;
+  height: 200px;
+  padding: 10px;
+  margin: 0 auto;
+  margin-top: 20px;
+  border-radius: 2%;
+  background-color: #e0e0e0;
+}
 </style>
