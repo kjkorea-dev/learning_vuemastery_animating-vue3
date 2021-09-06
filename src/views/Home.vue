f2c316178b71ccf42f69db9ea9b918ee59afae49
kj <agassi77@gmail.com>
2021-09-06 14:57:28 +0900

    Transitions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <button @click="toggleModal">Open</button>
-    <div v-if="isOpen" class="modal">
-      <button @click="toggleModal">Close</button>
-    </div>
+    <transition name="fade">
+      <div v-if="isOpen" class="modal">
+        <button @click="toggleModal">Close</button>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -21,3 +23,14 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+</style>
